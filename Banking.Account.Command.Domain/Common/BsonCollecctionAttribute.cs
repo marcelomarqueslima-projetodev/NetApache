2bc26e18e4748e4f56794974839071fc7634cbf6
Marcelo Lima <marcelomarqueslima.dev@gmail.com>
2022-04-16 04:14:05 -0300

    Criando microservice de commad mongoDb

---
+++ Banking.Account.Command.Domain/Common/BsonCollecctionAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace Banking.Account.Command.Domain.Common
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class BsonCollecctionAttribute : Attribute
+    {
+        public BsonCollecctionAttribute(string collectionName)
+        {
+            CollectionName = collectionName;
+        }
+
+        public string CollectionName { get; }
+
+
+    }
+}
