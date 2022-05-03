c0b5cad61c496d68bb7fa6e9e53c767495200839
Marcelo Lima <marcelomarqueslima.dev@gmail.com>
2022-05-02 23:19:41 -0300

    Finalizando a API

---
+++ Bank.Account.Query.Api/Program.cs
@@ -0,0 +1,33 @@
+using Banking.Account.Query.Application.Features.BankAccounts.Queries.FindAllAccounts;
+using Banking.Account.Query.Infrastructure;
+using Banking.Account.Query.Infrastructure.Consumers;
+using MediatR;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddMediatR(typeof(FindAllAccountsQuery).Assembly);
+
+builder.Services.AddSingleton<IHostedService, BankAccountConsumerService> ();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
